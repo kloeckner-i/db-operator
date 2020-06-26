390dbab62353720e850fa67ac03a94f0712d5dd6
Damian Peckett <damian.peckett@kloeckner.com>
2020-06-26 12:03:21 +0200

    [METAL-1957] Switch to password generator library

--- pkg/utils/kci/password_test.go
+++ pkg/utils/kci/password_test.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/kloeckner-i/can-haz-password/password"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,63 +13,11 @@ func TestGeneratePass(t *testing.T) {
 	generatedPassword := GeneratePass()
 
 	if assert.NotEmpty(t, generatedPassword) {
-		assert.Equal(t, 20, len(generatedPassword))
-		assert.Equal(t, 10, countOccurrences(generatedPassword, uppers+lowers))
-		assert.Equal(t, 8, countOccurrences(generatedPassword, digits))
-		assert.Equal(t, 2, countOccurrences(generatedPassword, specials))
+		assert.True(t, len(generatedPassword) >= 20)
+		assert.True(t, countOccurrences(generatedPassword, password.UppercaseCharacters+password.LowercaseCharacters) >= 10)
+		assert.True(t, countOccurrences(generatedPassword, password.DigitCharacters) >= 8)
+		assert.True(t, countOccurrences(generatedPassword, password.URLSafeSpecialCharacters) >= 2)
 	}
-}
-
-func TestSelectRandomCharacters(t *testing.T) {
-	src := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-	freq := make(map[rune]int)
-	for i := 0; i < 1000; i++ {
-		res, err := selectRandomCharacters(src, 5)
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, c := range res {
-			freq[c]++
-		}
-	}
-
-	for rune, n := range freq {
-		if n < 150 || n >= 250 {
-			t.Errorf("Unexpected outlier: rune = %d, count = %d", rune, n)
-		}
-	}
-}
-
-func TestSecureShuffle(t *testing.T) {
-	src := "The 0,u|ck brow/|/ f°? _|u^^ps °\\/er one3 lazj 1)°g$."
-
-	s1, err := secureShuffle(src)
-	if err != nil {
-		t.Error(err)
-	}
-
-	s2, err := secureShuffle(src)
-	if err != nil {
-		t.Error(err)
-	}
-
-	assert.Len(t, s1, len(src))
-	assert.Len(t, s2, len(src))
-	assert.Equal(t, runeFrequency(src), runeFrequency(s1))
-	assert.Equal(t, runeFrequency(src), runeFrequency(s2))
-	assert.NotEqual(t, src, s1)
-	assert.NotEqual(t, s1, s2)
-}
-
-// The number of occurances per rune in the source string.
-func runeFrequency(src string) map[rune]int {
-	freq := make(map[rune]int)
-	for _, c := range src {
-		freq[c]++
-	}
-	return freq
 }
 
 // The number of occurrences of a rune/s in a string.
