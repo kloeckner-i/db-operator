1a7ba0ea903b957e51c4ec96b07869528cae874b
Soohyun Kim <hyunysmile@gmail.com>
2021-11-17 16:12:58 +0100

    METAL-3189 remove mysql percona integration test

--- integration/test.sh
+++ integration/test.sh
@@ -64,10 +64,6 @@ create_test_resources() {
     echo "[Test] creating"
     $KUBECTL_CMD create ns ${TEST_NAMESPACE} --dry-run=client -o yaml | $KUBECTL_CMD apply -f - \
     && $HELM_CMD upgrade --install --namespace ${TEST_NAMESPACE} test-mysql-generic integration/mysql-generic --wait \
-    && $HELM_CMD dependency build --namespace ${TEST_NAMESPACE} integration/mysql-percona \
-    && $HELM_CMD upgrade --install --namespace ${TEST_NAMESPACE} test-mysql-percona integration/mysql-percona --wait --debug \
-    || kubectl get pod -n ${TEST_NAMESPACE} \
-    && kubectl logs -f test-mysql-percona-pxc-0 -c database -n ${TEST_NAMESPACE} \
     && $HELM_CMD upgrade --install --namespace ${TEST_NAMESPACE} test-pg-generic integration/postgres-generic --wait \
     && $HELM_CMD upgrade --install --namespace ${TEST_NAMESPACE} test-pg-gsql integration/postgres-gsql --wait
     if [ $? -ne 0 ]; then
